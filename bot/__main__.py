8ae88ad7031531e10b5f8c763906edce0d59dd3f
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2022-08-01 23:40:54 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
+    buttons.buildbutton("Leech/Mirror Group 🖤", "https://t.me/ashish_leech_group")
+    buttons.buildbutton("Owner ❤", "https://t.me/ashishsharmalegend")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
